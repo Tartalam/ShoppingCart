98a3e0af44f1fe99c3867abedba4298f5fcf6996
Tartalam <116926443+Tartalam@users.noreply.github.com>
2025-06-27 19:13:10 -0500

    javfx again

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,6 @@
-module ShoppingCart {
+module test12 {
 	requires javafx.controls;
+	requires javafx.graphics;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens Constroller to javafx.graphics, javafx.fxml;
 }
