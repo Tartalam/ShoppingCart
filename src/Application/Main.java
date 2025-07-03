4514a7fb28749153207508bd43846028d8a2062f
Tartalam <116926443+Tartalam@users.noreply.github.com>
2025-07-02 23:02:34 -0500

    design unified

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,17 +1,19 @@
 package Application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1366,768);
+			Parent root = FXMLLoader.load(getClass().getResource("MainPageGUI.fxml"));
+			//BorderPane root = new BorderPane();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,8 +21,9 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
+public static void main(String[] args) {		
+
+launch(args);
 	}
+
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module test12 {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens Application to javafx.graphics, javafx.fxml;
 }
